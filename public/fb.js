e2af5d90e84da0a4ae67c1f4fdb704ff1de382b9
ScribblerWriter <qrbraddock+github@gmail.com>
2018-12-05 10:43:43 +0100

    Moved Firebase declarations and code to a separate script file. Trimmed out the unneeded parts leftover from tutorials.

---
+++ public/fb.js
@@ -0,0 +1,15 @@
+(function() {
+
+	// initialize Firebase
+	const config = {
+		apiKey: "AIzaSyA7yISzO1I4rss7s1mykVmBjMAKGDExRjo",
+		authDomain: "writer-788cf.firebaseapp.com",
+		databaseURL: "https://writer-788cf.firebaseio.com",
+		projectId: "writer-788cf",
+		storageBucket: "writer-788cf.appspot.com",
+		messagingSenderId: "181941734046"
+	};
+    
+	firebase.initializeApp(config);
+
+}());
